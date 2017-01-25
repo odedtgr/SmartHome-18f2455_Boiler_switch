eae6979b7d83add028593557c56e30f628699e71
odedtgr <oded.tgr@gmail.com>
2017-01-25 14:13:57 +0200

    ready for testing

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+// Switch and 2 color indicator led for a remote boiler relay
+// states:
+//0 - Off
+//1 - On manual locally (red constant)
+//2 - On manual from remote (green constant)
+//3 - On Automatic locally (red blinking)
+//4 - On Automatic remote pre defined(green blinking)
+//5 - On Automatic remote by parameters(green blinking)
 #ifndef MAIN_C
 #define MAIN_C
 
@@ -27,25 +35,23 @@
 #define RedLed2     LATAbits.LATA1
 #define GreenLed1   LATAbits.LATA2
 #define GreenLed2   LATAbits.LATA3
-#define SRR         LATAbits.LATA5 
 #define Switch      PORTBbits.RB6 //Switch 1 input
 
 #define ADDRESS 3//XBee address.
+#define SERVER_ADDRESS 0x00
+#define RELAY_ADDRESS 0x08
 
 // Define the globals 
 #pragma udata
 int deviceState = 0;
 char ADDRH = 0x00;
-char ADDRL = 0x00;
+char ADDRL = 0x08;
 //UART variables
 char RxBuffer[100];
 char TxBuffer[100];
 int receivePos = 0;
 //boiler variables
 double time=0;
-double secondsCounter=0;
-double hours;
-double minutes;
 double SWcount = 0;
 unsigned char SWbuffer = 0;
 #pragma udata
@@ -61,7 +67,7 @@ void TransmitUsart(char device, char data);
 void TransmitUsartAT(void);
 void delay1sec(void);
 void handleMessge(void);
-void boilerCommands(char device);
+void boilerSwitchCommands(char device);
 
 
 #pragma code HIGH_INTERRUPT_VECTOR = 0x08
@@ -103,33 +109,24 @@ void highPriorityISRCode() {
 void lowPriorityISRCode() {
     int threshhold;
 
-    //TMR0 interrupt on overflow
+    //TMR0 interrupt on overflow, blink LED if boiler is on 
     if (INTCONbits.TMR0IF) {
         INTCONbits.TMR0IF = 0; //clear the interrupt flag
         TMR0H = 0x0B;
         TMR0L = 0xDC; //TMR0 Preload = 3036; Actual Interrupt Time : 1 sec
-        if (secondsCounter < hours*3600+minutes*60){
-            secondsCounter++;
-            if (deviceState ==1){
-                RedLed1 = !RedLed1;
-                RedLed2 = !RedLed2;
-            }
-            if (deviceState ==3){
-                // auto mode from remote
-                GreenLed1 = !GreenLed1;
-                GreenLed2 = !GreenLed2;
-            }
-        }else {
-            //time has passed
-            INTCONbits.TMR0IE = 0; //Timer0 interrupt on overflow
-            SRR = 0;
-            deviceState = 0; //off
-            TransmitUsart(0x01, (char) deviceState);
-            RedLed1 = 0;
-            RedLed2 = 0;
+        if (deviceState ==3){
+            RedLed1 = !RedLed1;
+            RedLed2 = !RedLed2;
             GreenLed1 = 0;
             GreenLed2 = 0;
-        }  
+        }
+        if (deviceState ==4){
+            // auto mode from remote
+            GreenLed1 = !GreenLed1;
+            GreenLed2 = !GreenLed2;
+            RedLed1 = 0;
+            RedLed2 = 0;
+        }
     }
 
     //TMR1 interrupt on overflow
@@ -146,50 +143,34 @@ void lowPriorityISRCode() {
             INTCONbits.RBIF = 0; //Flag must be cleared            
             INTCONbits.RBIE = 1; //Enable interrupt on change
             SWcount = 0; //reset the time counter
-            if(SRR){
-                INTCONbits.TMR0IE = 0; //Timer0 interrupt on overflow
-                SRR = 0;
+            if(!deviceState == 0){
+                T1CONbits.TMR1ON = 0; //Timer1 off, no need to keep reading the switch for debounce
                 deviceState = 0;//off
-                TransmitUsart(0x01, (char)deviceState);
-                RedLed1 = 0;
-                RedLed2 = 0;
-                GreenLed1 = 0;
-                GreenLed2 = 0;                
-            }else{
-                SRR = 1;
-                RedLed1 = 1;
-                RedLed2 = 1;
-                GreenLed1 = 0;
-                GreenLed2 = 0;
+                TransmitUsart(0x01, (char)deviceState);             
             }
-            
         }
 
         //debounced ON
-        if (SWbuffer == 0xff)
+        else if (SWbuffer == 0xff)
             SWcount++; // count timer ticks of debounced ON. 8ms
 
         //falling edge
-        if (SWbuffer == 0b11111110) {
+        else if (SWbuffer == 0b11111110) {
             T1CONbits.TMR1ON = 0; //Timer1 On bit
             INTCONbits.GIEL = 1; //Enable low priority interrupt
             INTCONbits.RBIF = 0; //Flag must be cleared
             INTCONbits.RBIE = 1; //Enable interrupt on change
 
-            if(SRR){
+            if(deviceState == 0){
                 // Handle output according to time button was pressed
                 threshhold = 1 * 1000 / 8; //1 sec in timer1 counts
                 if (SWcount < threshhold) {
                     //short button press - auto mode
-                    minutes = (double)Read_b_eep(0x01);
-                    hours = (double)Read_b_eep(0x02);
-                    secondsCounter=0;
-                    deviceState = 1;//Auto mode from switch
+                    deviceState = 3;//Auto mode from switch
                     TransmitUsart(0x01, (char)deviceState);
-                    INTCONbits.TMR0IE = 1; //Timer0 interrupt on overflow
                 }else{
                     //long button press - manual mode
-                    deviceState = 2;//manual mode from switch
+                    deviceState = 1;//manual mode from switch
                     TransmitUsart(0x01, (char)deviceState);
                 }                                   
             }
@@ -201,14 +182,7 @@ void lowPriorityISRCode() {
 void main(void) {
     //init device
     initialisePic();
-    initUsart();
-    
-    //init EEPROM with default values
-    if(Read_b_eep(0x01)==0xff)
-        Write_b_eep(0x01, (char)1 );//minutes
-    if(Read_b_eep(0x02)==0xff)
-        Write_b_eep(0x02, (char)0 );//hours
-    
+    initUsart();   
     initXbee();
         
     //Main loop
@@ -225,7 +199,7 @@ void handleMessage(void) {
         switch (RxBuffer[8]) //first data byte, Device selection
         {
             case 1://Device is Boiler
-                boilerCommands(RxBuffer[8]);
+                boilerSwitchCommands(RxBuffer[8]);
                 break;
 
             default: // Unknown command received
@@ -234,66 +208,54 @@ void handleMessage(void) {
     }
 }
 
-void boilerCommands(char device) {
+void boilerSwitchCommands(char device) {
     INTCONbits.GIEH = 1;//enable high priority interrupts. enable stopping shutters while on delay.
     switch (RxBuffer[9]) //Device specific command
     {
-        case 0://get device status
-            TransmitUsart(device, (char)deviceState);
-            break;
-
-        case 1://on pre defined timer
-            SRR = 1;
-            RedLed1 = 0;
-            RedLed2 = 0;
-            minutes = (double) Read_b_eep(0x01);
-            hours = (double) Read_b_eep(0x02);
-            secondsCounter = 0;
-            deviceState = 3; //Auto mode 
-            TransmitUsart(device, (char) 1);
-            INTCONbits.TMR0IE = 1; //Timer0 interrupt on overflow
-            break;
-            
-        case 2://on timer by parameters
-            SRR = 1;
-            RedLed1 = 0;
-            RedLed2 = 0;
-            minutes = (double)RxBuffer[10];
-            hours = (double) RxBuffer[11];
-            secondsCounter = 0;
-            deviceState = 3; //Auto mode 
-            TransmitUsart(device, (char) 1);
-            INTCONbits.TMR0IE = 1; //Timer0 interrupt on overflow
-            break;
-            
-        case 3://on manual
+        case 0://off
             INTCONbits.TMR0IE = 0; //Timer0 interrupt on overflow
-            SRR = 1;
-            RedLed1 = 0;
-            RedLed2 = 0;
-            GreenLed1 = 1;
-            GreenLed2 = 1;
-            deviceState = 2; //manual mode 
-            TransmitUsart(device, (char) deviceState); 
-            break;
-
-        case 4://off
-            INTCONbits.TMR0IE = 0; //Timer0 interrupt on overflow
-            SRR = 0;
             deviceState = 0; //off
-            TransmitUsart(device, (char) deviceState);
             RedLed1 = 0;
             RedLed2 = 0;
             GreenLed1 = 0;
             GreenLed2 = 0;    
             break;
-
-        case 5://Write EEPROM parameter
-            Write_b_eep(RxBuffer[10], RxBuffer[11]);
+            
+        case 1://on manual local
+            INTCONbits.TMR0IE = 0; //Timer0 interrupt on overflow
+            RedLed1 = 1;
+            RedLed2 = 1;
+            GreenLed1 = 0;
+            GreenLed2 = 0;
+            deviceState = 1; //manual local 
+        break;
+        
+        case 2://on manual remote
+            INTCONbits.TMR0IE = 0; //Timer0 interrupt on overflow
+            RedLed1 = 0;
+            RedLed2 = 0;
+            GreenLed1 = 1;
+            GreenLed2 = 1;
+            deviceState = 2; //manual local 
+        break;
+        
+        case 3://On Automatic locally
+            deviceState = 3; //Auto mode 
+            INTCONbits.TMR0IE = 1; //Timer0 interrupt on overflow
             break;
             
-        case 6://Read EEPROM parameter
-            TransmitUsart(RxBuffer[10], Read_b_eep(RxBuffer[10]));
+        case 4://On Automatic from remote predefined
+            deviceState = 4; //Auto mode 
+            INTCONbits.TMR0IE = 1; //Timer0 interrupt on overflow
+            break;
+            
+        case 5://On Automatic from remote by parameters
+            deviceState = 4; //Auto mode 
+            INTCONbits.TMR0IE = 1; //Timer0 interrupt on overflow
+            break;
+
+        case 10://get device status
+            TransmitUsart(device, (char)deviceState);
             break;
 
         default: // Unknown command received
